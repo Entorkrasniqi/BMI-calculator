aa61032bf80bc963cecf64cbe505e47a58fdd4e8
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2023-06-12 14:21:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example JavaScript code
+window.onload = function() {
+    // Access an HTML element by its ID
+    var myElement = document.getElementById("myElementId");
+    
+    // Manipulate the element
+    myElement.innerHTML = "Hello, JavaScript!";
+  };
+  
