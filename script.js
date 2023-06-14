bea96295fb594c3c736a6344e7acfeb02f6db8bc
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2023-06-14 14:33:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ window.onload = function() {
     // Manipulate the element
     myElement.innerHTML = "Hello, JavaScript!";
   };
+
+  function bmiCalculator (weight, height) {
+
+    var interpretation;
+    var bmi = Math.floor(weight/Math.pow(height,2));
+
+    if(bmi < 18.5){
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+    }
+
+    if (bmi > 18.5 && bmi <=  24.9){
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+    }
+
+    if (bmi > 24.9){
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+    return interpretation;
+
+}
   
