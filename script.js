79ce90546f84d2b5f296550f94a33bd1d903470a
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2023-06-15 12:41:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ window.onload = function() {
     myElement.innerHTML = "Hello, JavaScript!";
   };
 
+ // Example JavaScript code
+window.onload = function() {
+    // Access an HTML element by its ID
+    var myElement = document.getElementById("myElementId");
+    
+    // Manipulate the element
+    myElement.innerHTML = "Hello, JavaScript!";
+  };
+
   function bmiCalculator (weight, height) {
 
     var interpretation;
@@ -26,4 +35,22 @@ window.onload = function() {
     return interpretation;
 
 }
+
+function calculateBMI() {
+    var weightInput = document.getElementById("weight");
+    var heightInput = document.getElementById("height");
+    var resultElement = document.getElementById("result");
+
+    var weight = parseFloat(weightInput.value);
+    var height = parseFloat(heightInput.value);
+
+    if (isNaN(weight) || isNaN(height)) {
+        resultElement.textContent = "Please enter valid weight and height.";
+        return;
+    }
+
+    var interpretation = bmiCalculator(weight, height);
+    resultElement.textContent = interpretation;
+}
+
   
