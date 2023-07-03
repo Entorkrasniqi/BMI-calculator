b8e27d2b5b57d895baf7402c40952dcd9a99cfd5
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2023-07-03 18:57:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
+window.onload = function() {
+  // Access an HTML element by its ID
+  var myElement = document.getElementById("myElementId");
+  
+  // Manipulate the element
+  myElement.innerHTML = "Hello, JavaScript!";
+};
+
 // Example JavaScript code
 window.onload = function() {
-    // Access an HTML element by its ID
-    var myElement = document.getElementById("myElementId");
-    
-    // Manipulate the element
-    myElement.innerHTML = "Hello, JavaScript!";
-  };
+  // Access an HTML element by its ID
+  var myElement = document.getElementById("myElementId");
+  
+  // Manipulate the element
+  myElement.innerHTML = "Hello, JavaScript!";
+};
 
- // Example JavaScript code
-window.onload = function() {
-    // Access an HTML element by its ID
-    var myElement = document.getElementById("myElementId");
-    
-    // Manipulate the element
-    myElement.innerHTML = "Hello, JavaScript!";
-  };
+function bmiCalculator (weight, height) {
 
-  function bmiCalculator (weight, height) {
+  var interpretation;
+  var bmi = Math.floor(weight/Math.pow(height,2));
+  
+  if(bmi < 18.5){
+      interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+  }
 
-    var interpretation;
-    var bmi = Math.floor(weight/Math.pow(height,2));
+  if (bmi > 18.5 && bmi <=  24.9){
+      interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+  }
 
-    if(bmi < 18.5){
-        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
-    }
-
-    if (bmi > 18.5 && bmi <=  24.9){
-        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
-    }
-
-    if (bmi > 24.9){
-        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
-    }
-    return interpretation;
+  if (bmi > 24.9){
+      interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+  }
+  return interpretation;
 
 }
 
 function calculateBMI() {
-    var weightInput = document.getElementById("weight");
-    var heightInput = document.getElementById("height");
-    var resultElement = document.getElementById("result");
+  var weightInput = document.getElementById("weight");
+  var heightInput = document.getElementById("height");
+  var resultElement = document.getElementById("result");
 
-    var weight = parseFloat(weightInput.value);
-    var height = parseFloat(heightInput.value);
+  var weight = parseFloat(weightInput.value);
+  var height = parseFloat(heightInput.value);
 
-    if (isNaN(weight) || isNaN(height)) {
-        resultElement.textContent = "Please enter valid weight and height.";
-        return;
-    }
+  if (isNaN(weight) || isNaN(height)) {
+      resultElement.textContent = "Please enter valid weight and height.";
+      return;
+  }
 
-    var interpretation = bmiCalculator(weight, height);
-    resultElement.textContent = interpretation;
+  var interpretation = bmiCalculator(weight, height);
+  resultElement.textContent = interpretation;
 }
-
-  
